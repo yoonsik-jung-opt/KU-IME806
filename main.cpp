05a0d18be2355cc27375cd96a8f88f36636414c8
Yoonsik Jung <developer.kelvin@gmail.com>
2023-05-08 10:26:46 +0900

    first commit

---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include "pch.h"
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
---
+++ pch.h
@@ -0,0 +1,12 @@
+// PreCompiledHeader
+// this file is included only .cpp file
+#ifndef IME806_PCH_H
+#define IME806_PCH_H
+
+#include <iostream>
+#include "vector"
+#include "string"
+
+
+
+#endif //IME806_PCH_H
